fd5fdc059eb67c1ff26641f93a4da5b1326b633a
tayanovskii <tayanovskii@gmail.com>
2019-04-05 00:17:26 +0300

    complete ishop2

---
+++ ishop2/ShopItemsBlock.js
@@ -0,0 +1,79 @@
+var ShopItemsBlock = React.createClass({
+
+    displayName: 'ItemsBlock',
+
+    propTypes: {
+        shopName: React.PropTypes.string.isRequired,
+        items: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                code: React.PropTypes.number.isRequired,
+                name: React.PropTypes.string.isRequired,
+                price: React.PropTypes.number.isRequired,
+                photo_url: React.PropTypes.string.isRequired,
+                quantity: React.PropTypes.number.isRequired,
+            })
+        ).isRequired
+    },
+
+    getInitialState: function () {
+        return {
+            selectedItemCode: 0,
+            deletedItemCode: 0,
+            currentItems: this.props.items,
+        };
+    },
+
+    itemSelected: function (code) {
+        this.setState({
+            selectedItemCode: code
+        });
+    },
+
+    itemDeleted: function (code) {
+        this.setState({
+            deletedItemCode: code
+        }, this.deleteItem);
+
+    },
+
+    deleteItem: function () {
+        this.setState({
+            currentItems: this.state.currentItems.filter(v => v.code != this.state.deletedItemCode)
+        });
+    },
+
+    render: function () {
+
+
+        var itemsArr = this.state.currentItems.map((v) => {
+            return React.createElement(ShopItem, {
+                key: v.code,
+                code: v.code,
+                photo_url: v.photo_url,
+                name: v.name,
+                price: v.price,
+                quantity: v.quantity,
+                cbItemSelected: this.itemSelected,
+                selectedItemCode: this.state.selectedItemCode,
+                cbItemDeleted: this.itemDeleted,
+            })
+        })
+
+        return React.DOM.div({
+                className: 'ItemsBlock'
+            },
+            React.DOM.h2({}, this.props.shopName),
+            React.DOM.table({
+                    className: 'ItemsTable'
+                }, React.DOM.thead({}, React.DOM.tr({},
+                    React.DOM.th({}, 'Name'),
+                    React.DOM.th({}, 'Price'),
+                    React.DOM.th({}, 'URL'),
+                    React.DOM.th({}, 'Quantity'),
+                    React.DOM.th({}, 'Control')), ),
+                React.DOM.tbody({}, itemsArr))
+        )
+
+    },
+
+});
